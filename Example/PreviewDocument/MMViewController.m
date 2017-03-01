101a2708f0f894f3339647317bd42ef7c0cb8c66
Mao <mao@sc.10086.com.defaultbadlist>
2017-03-01 14:30:41 -0800

    添加使用例子
    Signed-off-by: Mao <mao@sc.10086.com.defaultbadlist>

--- Example/PreviewDocument/MMViewController.m
+++ Example/PreviewDocument/MMViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "MMViewController.h"
+#import "PreviewDocument.h"//导入类
 
 @interface MMViewController ()
 
@@ -17,7 +18,19 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *filePath=[[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask,YES)objectAtIndex:0]stringByAppendingPathComponent:[NSString stringWithFormat:@"%@",@"文件名"]];//获取你的文件所在沙盒的位置
+    PreviewDocument *preview = [[PreviewDocument alloc]initDocumentURL:filePath type:DocumentWordType];
+    /*
+    preview.url = @"这是一个新的路径文档";//获得新的路径文档
+    preview.type = DocumentPPTType; //设置新的类型
+     */
+    
+    /*
+     frame :预览文档的预览框的大小
+     controller:由哪个控制器来负责弹出和返回
+     */
+    [preview presentPreviewInview:self.view.frame controller:self Animated:YES];
 }
 
 - (void)didReceiveMemoryWarning
